8060638ece971d4eccbb55b7c5e46e501bacc9f0
SteepreE <steepreepubg@gmail.com>
2021-09-23 01:39:09 +0800

    Добавьте файлы проекта.

---
+++ SecondLab/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SecondLab
+{
+    class Calculator
+    {
+        public static int Calculate(String str)
+        {
+            int result = 0;
+            str = str.Replace(" ", "");
+
+            if (String.IsNullOrEmpty(str)) return result;
+
+            String[] elems = str.Split('+', '-');
+            result = int.Parse(elems[0]);
+            for (int i = 0; i < elems.Length; i++)
+            {
+                int value = int.Parse(elems[i]);
+                result = (i % 1 == 0) ? result + value : result - value;
+            }
+
+            return result;
+        }
+    }
+}
