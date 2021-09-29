8483b03e17c2540a86fd0aa3ac7b259184a0c080
YA <--->
2021-09-29 12:28:00 +0800

    create exeptions

--- SecondLab/Calculator.cs
+++ SecondLab/Calculator.cs
@@ -4,18 +4,18 @@ namespace SecondLab
 {
     class Calculator
     {
-        public static int Calculate(String str)
+        public static long Calculate(String str)
         {
-            int result = 0;
+            long result = 0;
             str = str.Replace(" ", "");
 
             if (String.IsNullOrEmpty(str)) return result;
 
             String[] elems = str.Split('+', '-');
-            result = int.Parse(elems[0]);
+            result = long.Parse(elems[0]);
             for (int i = 1; i < elems.Length; i++)
             {
-                int value = int.Parse(elems[i]);
+                var value = long.Parse(elems[i]);
                 result = (i % 2 == 1) ? result + value : result - value;
             }
 
--- SecondLab/Form1.cs
+++ SecondLab/Form1.cs
@@ -19,11 +19,41 @@ namespace SecondLab
 
         private void СalculateButton_Click(object sender, EventArgs e)
         {
+          
             String equation = EquationInputBox.Text;
+            try
+            {
+                if (IsOnlyNumbers(equation))
+                {
+                    var result = Calculator.Calculate(equation);
 
-            int result = Calculator.Calculate(equation);
+                    EquationInputBox.BackColor = Color.White;
+                    MessageBox.Show("Результат: " + result);
+                } else
+                {
+                    EquationInputBox.BackColor = Color.Red;
+                }
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+            
+        }
 
-            MessageBox.Show("Результат: " + result);
+        private bool IsOnlyNumbers(String str)
+        {
+            str = str.Replace(" ", "");
+            for (int i=0; i < str.Length; i++)
+            {
+                if (!Char.IsDigit(str[i]) && str[i]!='-' && str[i]!='+')
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
